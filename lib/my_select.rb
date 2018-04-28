d8588deb418d715614ff66d199a60e2fd0800656
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 05:46:07 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  selected_items = []
  while i < collection.size
-   if yield(collection[i])
+   if yield(collection[i]) ##if method that takes this block returns true
      selected_items << collection[i]
    end
    i = i + 1
