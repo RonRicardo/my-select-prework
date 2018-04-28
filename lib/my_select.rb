cd4c098f8211675c2c0bc7964e5cdb3518511c47
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 05:45:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
  i = 0
  selected_items = []
  while i < collection.size
-   selected_items << yield(collection[i])
+   if yield(collection[i])
+     selected_items << collection[i]
+   end
    i = i + 1
    end
   selected_items
